1e4effcd7e7d0a376ab4f618453a4910cabaf7b1
ProphesierC <bwknowles98@gmail.com>
2025-09-14 22:58:31 -0400

    update CardContainer.vue

--- app/src/Components/CardContainer.vue
+++ app/src/Components/CardContainer.vue
@@ -10,30 +10,36 @@
         class="rounded-t-4xl w-full h-auto"
       />
         <div id="card_inner_content"
-          class="h-100 w-full flex flex-col items-center rounded-b-4xl bg-white px-8 font-family-RH">
+          class="h-110 w-full flex flex-col items-center rounded-b-4xl bg-white px-8 font-family-RH">
 
           <h1
-            class="font-extrabold text-3xl py-6 tracking-tighter">
-              Order Summary
+            class="font-extrabold text-3xl py-8 tracking-tighter"
+            >Order Summary
           </h1>
 
-          <p class="text-md text-(--neutral-clr-Grey)">
-            You can now listen to millions of songs, audiobooks, and podcasts on any
+          <p class="text-md text-(--neutral-clr-Grey)"
+            >You can now listen to millions of songs, audiobooks, and podcasts on any
             device anywhere you like!
           </p>
 
-          <span class="flex items-center my-4 w-full h-20 px-4
+          <span class="flex items-center my-4 w-full h-22 px-4
             bg-[var(--clr-plan-background)] rounded-2xl">
             <img src="/src/assets/icon-music.svg">
             <div class="flex flex-col items-start pl-6">
-              <h2 class="font-extrabold">Annual Plan</h2>
-              <h3 class="text-(--neutral-clr-Grey)">$59.99/year</h3>
+              <h2 class="font-extrabold"
+                >Annual Plan</h2>
+              <h3 class="text-(--neutral-clr-Grey)"
+                >$59.99/year</h3>
             </div>
-            <button class="ml-auto underline text-[var(--primary-clr-Blue-700)] font-extrabold">Change</button>
+            <button class="ml-auto underline text-[var(--primary-clr-Blue-700)] font-extrabold cursor-pointer"
+              >Change</button>
           </span>
 
-          <div>Proceed to Payment</div>
-          <div>Cancel Order</div>
+          <div class="flex flex-row items-center justify-center bg-[var(--primary-clr-Blue-700)] text-white
+            w-full h-12 rounded-xl font-extrabold my-2 mb-6 cursor-pointer"
+            >Proceed to Payment</div>
+          <div class="text-(--neutral-clr-Grey) font-extrabold cursor-pointer"
+            >Cancel Order</div>
         </div>
 
     </div>
